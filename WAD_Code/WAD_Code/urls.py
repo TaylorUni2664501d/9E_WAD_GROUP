fec31098b6b74b1cc2eaeb92f4f32a935cebf49d
Taylor <tayloredouglas2020@gmail.com>
2023-03-02 21:28:02 +0000

    set up media files

--- WAD_Code/WAD_Code/urls.py
+++ WAD_Code/WAD_Code/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from manager import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('manager/', include('manager.urls')),
     # maps any URLS starting with manager/ to be handled by manager
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
