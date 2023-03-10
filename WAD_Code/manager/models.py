aa9189edf64ef8fc45fe5cf56b1fba03ae32cc82
Taylor <tayloruni2023@proton.me>
2023-03-10 15:41:08 +0000

    Refactored Player-Teams Relationship

--- WAD_Code/manager/models.py
+++ WAD_Code/manager/models.py
@@ -9,39 +9,17 @@ from WAD_Code.settings import STATIC_DIR
 # ManyToManyField: https://docs.djangoproject.com/en/2.1/ref/models/fields/#manytomanyfield
 # Choices: https://docs.djangoproject.com/en/2.1/ref/models/fields/#choices
 
-# helper method to name and organise user profile pictures 
-# MUST BE OUTSIDE CLASS - DJANGO SPECIFIC IMPLEMENTATION
-def user_directory_path(instance, filename):
-    return f"player_profile_pic/user_{instance.id}/{filename}"
-class Player(models.Model):
-    forename = models.CharField(max_length=32)
-    surname = models.CharField(max_length=32)
-    age = models.IntegerField(default=0)
-    location = models.CharField(max_length=64)
-    bio = models.TextField(max_length=500)
-    # image fields allow users to enter an image in the form, and have it automatically uploaded to the correct media directory
-    # https://docs.djangoproject.com/en/2.1/ref/models/fields/#imagefield
-    profile_pic = models.ImageField(upload_to=user_directory_path, default=f"{STATIC_DIR}/images/default_profile.jpeg")
 
-    def __str__(self):
-        return f"{self.forename} {self.surname}"
 
 # helper method to name and organise team logos
 # MUST BE OUTSIDE CLASS - DJANGO SPECIFIC IMPLEMENTATION
 def logo_directory_path(instance, filename):
     return f"team_logo/team_{instance.team_name}/{filename}"
 class Team(models.Model):
-    def gallery_default(self): #helper class to make a per-entry default value pointing to the team's gallery folder - ideally should not need changed
+    def gallery_default(self): #helper method to make a per-entry default value pointing to the team's gallery folder - ideally should not need changed
         return f"team_gallery/team_{self.team_name}/"
     
     team_name = models.CharField(max_length=64, unique=True)
-    # many-to-many field via Team_Players, allows Team to contain players and hopefully simplify implementation while keeping extra relationship data
-    # https://docs.djangoproject.com/en/2.1/ref/models/fields/#manytomanyfield
-    registered_players = models.ManyToManyField( 
-        Player,
-        through="Team_Players",
-        through_fields=("team_id", "player_id")
-    )
     location = models.CharField(max_length=64)
     age_range = models.CharField(max_length=5)
     win_rate = models.FloatField(default=0.0)
@@ -51,6 +29,25 @@ class Team(models.Model):
 
     def __str__(self):
         return f"{self.team_name}"
+    
+# helper method to name and organise user profile pictures
+# MUST BE OUTSIDE CLASS - DJANGO SPECIFIC IMPLEMENTATION
+def user_directory_path(instance, filename):
+    return f"player_profile_pic/user_{instance.id}/{filename}"
+
+class Player(models.Model):
+    forename = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    age = models.IntegerField(default=0)
+    location = models.CharField(max_length=64)
+    bio = models.TextField(max_length=500)
+    # image fields allow users to enter an image in the form, and have it automatically uploaded to the correct media directory
+    # https://docs.djangoproject.com/en/2.1/ref/models/fields/#imagefield
+    profile_pic = models.ImageField(upload_to=user_directory_path, default=f"{STATIC_DIR}/images/default_profile.jpeg")
+    registered_team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.forename} {self.surname}"
 
 class Match(models.Model):
     team1_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="+") #challenging team
@@ -71,14 +68,6 @@ class Match(models.Model):
 
     def __str__(self):
         return f"{self.team1_id} vs {self.team2_id}"
-
-class Team_Players(models.Model): # many-to-many intermediary table
-    team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
-    player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
-    date_joined = models.DateField()
-
-    def __str__(self):
-        return f"{self.team_id} player {self.player_id}"
 
 class Team_Request(models.Model):
     player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
