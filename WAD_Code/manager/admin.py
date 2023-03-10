a8beac91582cd4e8a1965c6f69ed1a987057a15b
Taylor <tayloruni2023@proton.me>
2023-03-10 16:24:55 +0000

    remove deleted model import

--- WAD_Code/manager/admin.py
+++ WAD_Code/manager/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from manager.models import Player, Team, Match, Team_Players, Team_Request
+from manager.models import Player, Team, Match, Team_Request
 
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(Team)
 admin.site.register(Match)
-admin.site.register(Team_Players)
 admin.site.register(Team_Request)
