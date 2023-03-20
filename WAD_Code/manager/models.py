0fd79dc624265039ab53942ed04d9c0ab1406392
Taylor <tayloruni2023@proton.me>
2023-03-20 17:42:49 +0000

    update Match __str__

--- WAD_Code/manager/models.py
+++ WAD_Code/manager/models.py
@@ -79,7 +79,7 @@ class Match(models.Model):
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default="PN")
 
     def __str__(self):
-        return f"{self.team1_id} vs {self.team2_id}"
+        return f"{self.team1_id} vs {self.team2_id} at {self.date}"
 
 class Team_Request(models.Model):
     player_id = models.ForeignKey(Player, on_delete=models.CASCADE)
