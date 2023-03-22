4dfe90f1bd4e728ee79bf45a43d8c1305c5ddbf3
Milo Critchett <2651413c@student.gla.ac.uk>
2023-03-22 15:31:45 +0000

    Search

--- WAD_Code/manager/views.py
+++ WAD_Code/manager/views.py
@@ -48,6 +48,27 @@ def view_team(request, team_name):
 
     return render(request, 'manager/view_team.html', context=context_dict)
 
+def search_team(request):
+    if request.method == 'POST':
+		team_name = request.POST.get('team name')
+    context_dict = {}
+    context_dict['teams'] = search_list
+    return render(request, 'manager/search_teams.html', context = context_dict)
+#should render a page with search box, wait for input and pass 
+#the search term to search_results
+
+def seach_results(request, search):
+    team_list = Team.objects.order_by('team_name')
+    search_list = {}
+    for team in team_list
+        if (search in team.team_name)
+            search_list.append(team)
+    context_dict['teams'] = search_list
+	context_dict['search'] = search
+    return render(request, 'manager/search_results.html', context = context_dict)
+#recieves a search term from search_team and provides a
+#list of teams that match that term
+
 
 def signup_team(request):
     registered = False
